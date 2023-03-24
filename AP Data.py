16b49250a45fd874f7283490a58a9803ebf7bac4
Annie <ablischer@icloud.com>
2023-03-24 12:49:51 -0400

    hi

---
+++ AP Data.py
@@ -0,0 +1,65 @@
+import sqlib
+con = sqlib.create_db_connection("127.0.0.1","alischer","alischer1","in_class_demo")
+
+import sqlib
+import random
+data = []
+
+con = sqlib.create_db_connection("127.0.0.1","alischer","alischer1","in_class_demo")
+
+class movement:
+    id = None
+    grade = None
+    pet = None
+    sib = None
+    ice = None
+
+    def __init__(self, id):
+        grade = random.randint(5,12)
+        if grade == 5:
+            grade = "5th"
+        elif grade == 6:
+            grade = "6th"
+        elif grade == 7:
+            grade = "7th"
+        elif grade == 8:
+            grade = "8th"
+        elif grade == 9:
+            grade = "9th"
+        elif grade == 10:
+            grade = "10th"
+        elif grade == 11:
+            grade = "11th"
+        elif grade == 12:
+            grade = "12th"
+        self.grade = grade
+        pet = random.randint(1,4)
+        if pet == 1:
+            pet = "dog"
+        elif pet == 2:
+            pet = "cat"
+        elif pet == 3:
+            pet = "fish"
+        elif pet == 4:
+            pet = "reptile"
+        self.pet = pet
+        sib = random.randint(0,4)
+        self.sib = sib
+        ice = random.randint(1,4)
+        if ice == 1:
+            ice = "chocolate"
+        elif ice == 2:
+            ice = "vanilla"
+        elif ice == 3:
+            ice = "strawberry"
+        elif ice == 4:
+            ice = "mint"
+        self.ice = ice
+    
+    def __str__(self):
+        self = [self.id, self.grade, self.pet, self.sib, self.ice]
+
+for x in range(1, 101):
+    movementx = movement(x)
+    move = """insert into annie_ap (id,grade,pet,sib,ice) values ('{id}','{grade}','{pet}','{sib}','{ice}');""".format(id = movementx.id, grade = movementx.grade, pet = movementx.pet, sib = movementx.sib, ice = movementx.ice)
+    move_result = sqlib.execute_query(con, move)
---
+++ AP Project.py
@@ -0,0 +1,66 @@
+import sqlib
+con = sqlib.create_db_connection("127.0.0.1","alischer","alischer1","in_class_demo")
+
+# Gets All Possible Answers From Catagory(ies) and Gives How Many People Chose What
+def basic_data():
+    print("-----------------------------------------------------------------------------------------------------------------------------------")
+    print("""Questions posed to the students are under 4 main categories:
+1: Grade
+2: Pets
+3: Siblings
+4: Icecream""")
+    print("-----------------------------------------------------------------------------------------------------------------------------------")
+
+    cats = input("Please enter the coresponding number to you would like to learn more about separated by a space: ")
+    cats = list(map(int,cats.strip().split()))
+    # make sure its just one, get data, display good
+
+# To compare data from different catagories
+def compare_data():
+    print("-----------------------------------------------------------------------------------------------------------------------------------")
+    print("""Questions posed to the students are under 4 main categories:
+1: Grade
+2: Pets
+3: Siblings
+4: Icecream""")
+    print("-----------------------------------------------------------------------------------------------------------------------------------")
+
+    cats = input("Please enter the coresponding numbers of the 2 catagories you would like to compare separated by a space: ")
+    cats = list(map(int,cats.strip().split()))
+    # make sure tis just two, get data, display good
+
+# Graph data
+def graph_data():
+    print("-----------------------------------------------------------------------------------------------------------------------------------")
+    print("""Questions posed to the students are under 4 main categories:
+1: Grade
+2: Pets
+3: Siblings
+4: Icecream""")
+    print("-----------------------------------------------------------------------------------------------------------------------------------")
+
+    cats = input("Please enter the coresponding number of the catagor you would like to graph separated by a space: ")
+    # make sure its just one, get data, display good
+
+# End of graph_data func
+
+print("This code uses randomly generated data.")
+print("-----------------------------------------------------------------------------------------------------------------------------------")
+print("""There are 3 ways in which you can choose to display the data:
+1: Written Data
+2: Compare Data
+3: Graph Data""")
+      
+choice = int(input("Enter the coresponding number of the method you would like to use: "))
+
+try:
+    if choice == 1:
+        basic_data()
+    elif choice == 2:
+        compare_data()
+    elif choice == 3:
+        graph_data()
+    else:
+        print("that was not an option")
+except:
+    print("that was not an option")
