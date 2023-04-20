e754b85a62126fd1ce2e4592f36e74bb52f1209a
Annie <ablischer@icloud.com>
2023-04-20 12:19:48 -0400

    Added Comments

--- AP_joke.py
+++ AP_joke.py
@@ -1,23 +1,20 @@
-## User Input
-## At least one list
-## A function
-## if loop
-## for/while loop
-## call the function
-## output something
-
+# Gets user input
 word = str(input("Enter a word or sentence: "))
+# Turns input into a list
 word = list(word.upper())
 
+# Counts the number of characters in the input
 def count(word):
     number = 0
     for x in word:
         number += 1
     return(number)
 
+# Checks that the input isn't too short or too long
 if len(word) == 0:
     print("You didn't input anything.")
 elif len(word) >= 100:
     print("That is too many characters.")
+# outputs the number of characters in the input
 else:
     print("Your input has " + str(count(word)) + " characters.")
